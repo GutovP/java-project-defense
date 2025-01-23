3b9f1a92ada9cb7fb59a9882162fffa29f504cbb
Petar Gutov <pgutov@gmail.com>
2025-01-23 15:03:02 +0100

    first commit

---
+++ src/main/java/org/javaprojectdefense/JavaProjectDefenseApplication.java
@@ -0,0 +1,13 @@
+package org.javaprojectdefense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectDefenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectDefenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/javaprojectdefense/JavaProjectDefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaprojectdefense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectDefenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
