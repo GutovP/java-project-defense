86f1f569776e7ad23f9632ae682799ccd143492c
Petar Gutov <pgutov@gmail.com>
2025-03-04 10:54:09 +0100

    login dto created

---
+++ src/main/java/flower_shop/web/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package flower_shop.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @Email
+    private String email;
+
+    @Size(min = 6, max = 20, message = "Password length must be between 3 and 20 characters!")
+    private String password;
+}
