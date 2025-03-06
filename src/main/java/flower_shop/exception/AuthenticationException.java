598717dc3990cc299f2d6408f82a4ad0ed2f4ddd
Petar Gutov <pgutov@gmail.com>
2025-03-06 20:34:24 +0100

    auth and user not found exceptions created

---
+++ src/main/java/flower_shop/exception/AuthenticationException.java
@@ -0,0 +1,8 @@
+package flower_shop.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
