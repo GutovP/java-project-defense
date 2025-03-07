cbcd4a95dcf92c5a4e90f680d7df2813cab8b717
Petar Gutov <pgutov@gmail.com>
2025-03-07 11:20:44 +0100

    additional filed added to the dto

--- src/main/java/flower_shop/web/dto/ProfileEditRequest.java
+++ src/main/java/flower_shop/web/dto/ProfileEditRequest.java
@@ -1,5 +1,6 @@
 package flower_shop.web.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,6 +12,9 @@ public class ProfileEditRequest {
 
     @Size(min = 3, max = 20, message = "Last name length must be between 3 and 20 characters!")
     private String lastName;
+
+    @Email
+    private String email;
 
     @Size(min = 6, max = 20, message = "Password length must be between 6 and 20 characters!")
     private String password;
