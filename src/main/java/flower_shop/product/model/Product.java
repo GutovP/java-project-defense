6a4170d46ea2c020c717ee4daf7ba134bd78a3b3
Petar Gutov <pgutov@gmail.com>
2025-03-20 10:40:42 +0100

    restrictions set to the product name in the entity

--- src/main/java/flower_shop/product/model/Product.java
+++ src/main/java/flower_shop/product/model/Product.java
@@ -18,6 +18,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
     private String description;
