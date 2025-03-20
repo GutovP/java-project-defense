374a806ffe34adb57e1f020621447552be6efea0
Petar Gutov <pgutov@gmail.com>
2025-03-20 15:58:57 +0100

    request mapping added and getProduct() created

--- src/main/java/flower_shop/web/ProductController.java
+++ src/main/java/flower_shop/web/ProductController.java
@@ -3,11 +3,16 @@ package flower_shop.web;
 import flower_shop.product.service.ProductService;
 import flower_shop.web.dto.ProductResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/products")
+import static flower_shop.web.Paths.API_V1_BASE_PATH;
+
+@RestController
+@RequestMapping(API_V1_BASE_PATH + "/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -18,6 +23,15 @@ public class ProductController {
 
     @GetMapping("/all")
     public List<ProductResponse> getAllProducts() {
+
         return productService.getAllProducts();
     }
+
+    @GetMapping("/{category}/{name}")
+    public ProductResponse getProduct(@PathVariable String category, @PathVariable String name) {
+
+        return productService.getProduct(category, name);
+    }
+    
+
 }
