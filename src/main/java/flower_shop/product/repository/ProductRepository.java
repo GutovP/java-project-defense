c36a09a9b60075d8e48fdb927fb5d1eab32a8c99
Petar Gutov <pgutov@gmail.com>
2025-03-26 16:20:34 +0100

    product repository updated with mothod findByCategoryAndName

--- src/main/java/flower_shop/product/repository/ProductRepository.java
+++ src/main/java/flower_shop/product/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package flower_shop.product.repository;
 import flower_shop.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     Optional<Product> findByName(String name);
+
+    Optional<Product> findByCategoryAndName(String name, @PathVariable String category);
 }
