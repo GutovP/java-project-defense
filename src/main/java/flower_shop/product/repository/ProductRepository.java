403f56d53460d0a5347289cc41c44e86b8f1616a
Petar Gutov <pgutov@gmail.com>
2025-04-02 14:16:11 +0200

    find by current quantity less than threshold method implemented in product repository

--- src/main/java/flower_shop/product/repository/ProductRepository.java
+++ src/main/java/flower_shop/product/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package flower_shop.product.repository;
 
 import flower_shop.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,4 +16,7 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     Optional<Product> findByName(String name);
 
     Optional<Product> findByCategoryAndName(String name, @PathVariable String category);
+
+    @Query("SELECT p FROM Product p WHERE p.currentQuantity <= p.restockThreshold")
+    List<Product> findByCurrentQuantityLessThanThreshold();
 }
