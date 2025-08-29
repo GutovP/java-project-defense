d2ad52f72f25ee3d34ea0232ec1240f57e46bce9
Petar Gutov <pgutov@gmail.com>
2025-08-29 13:29:22 +0200

    finder query created for method findAllCatergories

--- src/main/java/flower_shop/product/repository/ProductRepository.java
+++ src/main/java/flower_shop/product/repository/ProductRepository.java
@@ -20,5 +20,6 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     @Query("SELECT p FROM Product p WHERE p.currentQuantity <= p.restockThreshold")
     List<Product> findByCurrentQuantityLessThanThreshold();
 
+    @Query("SELECT DISTINCT p.category FROM Product p")
     List<String> findAllCategories();
 }
